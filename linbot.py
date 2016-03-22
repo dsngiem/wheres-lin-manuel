974569e84cbb51283bd38e416ddfa76b11bd5c44
dsngiem <dsngiem@gmail.com>
2016-03-21 23:12:54 -0400

    ready for heroku deploment

--- linbot.py
+++ linbot.py
@@ -4,3 +4,41 @@
 # polls @lin_manuel's account for his location every minute
 # if it changes, it posts to @linmanuelishere
 
+import tweepy
+
+consumerKey = "8yfryf5JvvjeBOG4mUs42fWWg"
+consumerSecret = "yHGOeK1iLgq8IpW0pz7SYXi2W2oXGHORrqEMkGgKtjfiHw1Klk"
+
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+
+accessToken = "712089131778703360-MxbfGqF9oSHTvnBvVknWmTiHc1Y3ojl"
+accessTokenSecret = "xX1pFsMga44heuogml8snp8zPFW0PTDAy3jPLWQ7XePax"
+
+auth.set_access_token(accessToken, accessTokenSecret)
+
+api = tweepy.API(auth)
+
+def updateStatus(api):
+
+    #get lin location
+    linUser = api.get_user("lin_manuel")
+    linLocation = linUser.location.strip()
+
+    #get my last tweet
+    myTimeline = api.user_timeline("whereslinmanuel", count=1)
+    
+    if len(myTimeline) > 0:
+        myLastLocation = myTimeline[0].text.strip()
+    else:
+        myLastLocation = u""
+
+
+    if linLocation != myLastLocation:
+        print "update my status. current location:", type(myLastLocation), type(linLocation)
+
+        api.update_status(linLocation)
+    else:
+        print "location has not changed from:", myLastLocation
+
+updateStatus(api)
+
